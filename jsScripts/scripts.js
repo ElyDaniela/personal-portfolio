40599c5c4ebf37c81c2bb36cc60ac5df095420a2
Ely Islas <ely25a.m@gmail.com>
2020-03-21 10:55:52 -0600

    todo bien

---
+++ jsScripts/scripts.js
@@ -0,0 +1,31 @@
+$.ajax({
+    url:'data/profile.json',
+    method:'GET',
+    datatype:'JSON',
+    data:'',
+    beforeSend: function(){
+
+    },
+    success: function(data){
+        // alert(data.services.description);
+        $('#descServices').html(data.services.description);
+
+        data.services.listServices.forEach(element => {
+            let card = '<div class="col-md-6">'+
+                            '<div class="card">'+
+                            '<div class="card-body">'+
+                                '<h5 class="card-title">' +element.title+ '</h5>'+
+                                '<p class="card-text">' +element.description+ '</p>'+
+                                '<a href="#" class="btn btn-info">Read more</a>'+
+                            '</div>'+
+                            '</div>'+
+                        '</div>';
+
+            $('#contentServices').append(card);            
+           
+        });
+    },
+    error: function(){
+
+    }
+});
